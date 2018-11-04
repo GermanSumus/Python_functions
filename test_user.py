247a53772fb30adac76942d6ae0734d1e0a83159
German Rodriguez <germanrodriguez@germans-mini.attlocal.net>
2018-11-03 20:56:27 -0500

    Simple test function added. Returns boolean from user input.

---
+++ test_user.py
@@ -0,0 +1,15 @@
+from decimal import *
+
+
+# Test user input for Conformation/Continuation
+def test():
+    try:
+        test = input('Are you sure? ("N" or Enter to continue)\n> ')
+        print(test)
+        if test.upper() == 'Y' or test == '':
+            return True
+        elif test.upper() == 'N':
+            return False
+    except:
+        return False
+
